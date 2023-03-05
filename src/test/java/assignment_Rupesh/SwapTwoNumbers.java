732b1373588b772377ff98253205b3459a702b31
Rupesh01 <optimusrupesh@gmail.com>
2023-03-05 17:37:40 +0530

    Assignment commit

---
+++ src/test/java/assignment_Rupesh/SwapTwoNumbers.java
@@ -0,0 +1,12 @@
+package assignment_Rupesh;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 2;
+        int swap=a;
+        a=b;
+        b=swap;
+        System.out.println("a value is : " + a + " and b value is : " + b);
+    }
+}
