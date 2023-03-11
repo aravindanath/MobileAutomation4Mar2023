e8bf89af8ba844b2f18828cb46abbc786c05ed75
PreetiPandey28 <pandey28.preeti@gmail.com>
2023-03-11 23:44:50 +0000

    adding my new assignmnet

---
+++ src/test/java/preeti/AssignmentPrintNumber.java
@@ -0,0 +1,25 @@
+package preeti;
+
+public class AssignmentPrintNumber {
+    public static void main(String[] args) {
+        int num = 100;
+        for(int i=1;i<=num; i++)
+        {
+            System.out.println(i);
+
+            if (i % 3 == 0)
+            {
+                System.out.println("Fizz");
+            }
+            if (i % 5 == 0)
+            {
+                System.out.println("Buzz");
+            }
+            if (i % 3 == 0 && i % 5 == 0)
+            {System.out.println("FizzBuzz");
+            }
+            }
+        }
+
+    }
+
